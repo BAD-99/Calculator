a00a4eeaf993686e22a45e6bbecc6487898a8693
BAD-99 <brandon.andrew.davis@gmail.com>
2022-09-02 23:09:15 -0500

    Added script js and data attributes

---
+++ script.js
@@ -0,0 +1,7 @@
+class Calculator{
+
+}
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const functionButtons = document.querySelectorAll('[data-function]')
+const operatorButtons = document.querySelectorAll('[data-operator]')
